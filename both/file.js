bbe6d61683a75e841f596eef6e671e1df9a04dd7
BonaOng <bonasmart9@gmail.com>
2019-01-22 10:28:39 +0700

    on path

--- both/file.js
+++ both/file.js
@@ -8,12 +8,12 @@ storagePath: () => {
 // mkdir -p /data/my_upload
 // chmod -R 777 /data
 // Server path
-return '/data/my_upload' // Mac, Ubuntu
+// return '/data/my_upload' // Mac, Ubuntu
 // return 'public/meteor_uploads'
 // return 'C:\\data\\file_uploads' // Windows
 // return `${Meteor.rootPath}/file_uploads` // .meteor/local/programs/server/
 // return `${process.env.PWD}/meteor_uploads` // app path
-// return `${Meteor.absolutePath}/public/meteor_uploads` // app path
+return `${Meteor.absolutePath}/public/meteor_uploads` // app path
 },
 collectionName: 'file',
 // permissions: 777,
